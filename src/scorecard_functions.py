bf5a80a754711412b8b892473d1badbc327c58f8
General <kexiangen@outlook.com>
2019-06-04 11:58:26 +0800

    update

--- src/scorecard_functions.py
+++ src/scorecard_functions.py
@@ -3,7 +3,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import model_selection 
+from sklearn import model_selection, metrics
+
 
 def SplitData(df, col, numOfSplit, special_attribute=[]):
     '''
